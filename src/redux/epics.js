5b5dbe05e431f2bd6ead50bff95072435255f0f8
Sheng Chuan (Michael) Hsu <cool96482@hotmail.com>
2021-04-12 16:28:21 +0800

    remove some file

--- src/redux/epics.js
+++ src/redux/epics.js
@@ -1,7 +1,6 @@
 import { combineEpics } from 'redux-observable';
 
-import * as article from '@/redux/slices/article';
 import * as auth from '@/redux/slices/auth';
 import { getEpicsFromSlices } from './utils';
 
-export default combineEpics(...getEpicsFromSlices([article, auth]));
+export default combineEpics(...getEpicsFromSlices([auth]));
--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -2,13 +2,11 @@ import { combineReducers } from 'redux';
 
 import { connectRouter } from 'connected-react-router';
 
-import article from '@/redux/slices/article';
 import auth from '@/redux/slices/auth';
 
 const reducers = history =>
   combineReducers({
     router: connectRouter(history),
-    article,
     auth,
   });
 
